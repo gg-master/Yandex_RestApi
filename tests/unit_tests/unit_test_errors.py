2362e9674996b801fbbd54939d800545eca84a35
gg-master <korshun.tolik@yandex.ru>
2022-06-21 18:56:56 +0300

    tested unit_tests

--- tests/unit_tests/unit_test_errors.py
+++ tests/unit_tests/unit_test_errors.py
@@ -8,7 +8,8 @@ import urllib.error
 import urllib.parse
 import urllib.request
 
-API_BASEURL = "http://localhost:8081"
+API_BASEURL = "https://flows-1989.usr.yandex-academy.ru"
+# API_BASEURL = "http://localhost:8081"
 
 ROOT_ID = "069cb8d7-bbdd-47d3-ad8f-82ef4c269df1"
 
@@ -361,10 +362,8 @@ def test_delete():
 
 
 def test_all():
-    # test_import_bad_req()
+    test_import_bad_req()
     test_nodes_bad()
-    # test_sales()
-    # test_stats()
     # test_delete()
 
 
